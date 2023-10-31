0fbd7939308a9de628c9135770732ff3ebe6fa84
mm-fiserv <147175865+mm-fiserv@users.noreply.github.com>
2023-10-31 14:57:29 -0400

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,12 +1,30 @@
-class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
-    }
-  
-    public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+public class Solution
+{
+    public static char mostFrequentChar(String s)
+    {
+        char mostFrequentChar = s.charAt(0);
+        int maxCount = 0;
+        int[] charCount = new int[256];
+
+        for (char c : s.toCharArray())
+        {
+            charCount[c]++;
+
+            if (charCount[c] > maxCount || (charCount[c] == maxCount && s.indexOf(c) < s.indexOf(mostFrequentChar)))
+            {
+                maxCount = charCount[c];
+                mostFrequentChar = c;
+            }
+        }
+
+        return mostFrequentChar;
     }
 
-  }
+    public static void main(String[] args)
+    {
+        System.out.println(Solution.mostFrequentChar("bookeeper"));
+        System.out.println(Solution.mostFrequentChar("david"));
+        System.out.println(Solution.mostFrequentChar("mississippi"));
+        System.out.println(Solution.mostFrequentChar("eleventennine"));
+    }
+}
